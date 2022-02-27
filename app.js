1f27129666f91fed47877645672056d71011df5f
henryzhang03 <henryzhang1719@gmail.com>
2022-02-26 22:07:32 -0800

    Version before Github Pages hosting

--- app.js
+++ app.js
@@ -1,56 +1,278 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.navbar__menu');
-const navLogo = document.querySelector('#navbar__logo');
+let delayed;
+const pHctx = document.getElementById('pHChart').getContext('2d');
+const pHChart = new Chart(pHctx, {
+type: 'bar',
+data: {
+  labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  datasets: [{
+    label: 'Average pH',
+    data: [6.8, 7.2, 8.1, 7.9, 8.5, 7.1, 6.2],
+    backgroundColor: [
+      'rgba(54, 162, 235, 0.8)',
+      'rgba(54, 162, 235, 0.8)',
+      'rgba(54, 162, 235, 0.8)',
+      'rgba(54, 162, 235, 0.8)',
+      'rgba(54, 162, 235, 0.8)',
+      'rgba(54, 162, 235, 0.8)',
+      'rgba(54, 162, 235, 0.8)',
+    ],
+    borderColor: [
+      'rgba(54, 162, 235, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(54, 162, 235, 1)',
+      'rgba(54, 162, 235, 1)'
+    ],
+    borderWidth: 1
+  }]
+    
+},
+options: {
+  plugins: {
+    title: {
+      display: true,
+      text: 'pH',
+      font: {
+        size: 40,
+        family: 'raleway',
+        weight: 'normal',
+      },
+    }
+  },
+  animation: {
+    onComplete: () => {
+      delayed = true;
+    },
+    delay: (context) => {
+      let delay = 0;
+      if (context.type === 'data' && context.mode === 'default' && !delayed) {
+        delay = context.dataIndex * 300 + context.datasetIndex * 100;
+      }
+      return delay;
+    },
+  },
+  scales: {
+    y: {
+      beginAtZero: true,
+      suggestedMax: 10
+    }
+  }
+}
+});
 
-// Display Mobile Menu
-const mobileMenu = () => {
-  menu.classList.toggle('is-active');
-  menuLinks.classList.toggle('active');
+const oxygenctx = document.getElementById('oxygenChart').getContext('2d');
+  const oxygenChart = new Chart(oxygenctx, {
+  type: 'bar',
+  data: {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      datasets: [{
+          label: 'Average Dissolved Oxygen (mg/L)',
+          data: [6.61, 7.13, 8.09, 7.74, 7.32, 6.51, 6.78],
+          backgroundColor: [
+              'rgba(255, 99, 132, 0.8)',
+              'rgba(255, 99, 132, 0.8)',
+              'rgba(255, 99, 132, 0.8)',
+              'rgba(255, 99, 132, 0.8)',
+              'rgba(255, 99, 132, 0.8)',
+              'rgba(255, 99, 132, 0.8)',
+              'rgba(255, 99, 132, 0.8)',
+          ],
+          borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(255, 99, 132, 1)',
+              'rgba(255, 99, 132, 1)'
+          ],
+          borderWidth: 1
+      }]
+  },
+  
+  options: {
+    plugins: {
+      title: {
+        display: true,
+        text: 'Dissolved Oxygen',
+        font: {
+          size: 40,
+          family: 'raleway',
+          weight: 'normal',
+        },
+      }
+    },
+    animation: {
+      onComplete: () => {
+        delayed = true;
+      },
+      delay: (context) => {
+        let delay = 0;
+        if (context.type === 'data' && context.mode === 'default' && !delayed) {
+          delay = context.dataIndex * 300 + context.datasetIndex * 100;
+        }
+        return delay;
+      },
+    },
+    scales: {
+          y: {
+              beginAtZero: true,
+              suggestedMax: 10
+          }
+      }
+  }
+});
+
+const ctxturbidity = document.getElementById('turbidityChart').getContext('2d');
+    const myChart3 = new Chart(ctxturbidity, {
+    type: 'bar',
+    data: {
+        labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+        datasets: [{
+            label: 'Average Turbidity (NTU)',
+            data: [0.12, 0.2, 0.17, 0.14, 0.15, 0.1, 0.19],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+        
+    },
+    options: {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Turbidity',
+          font: {
+            size: 40,
+            family: 'raleway',
+            weight: 'normal',
+          },
+        }
+      },
+      animation: {
+        onComplete: () => {
+          delayed = true;
+        },
+        delay: (context) => {
+          let delay = 0;
+          if (context.type === 'data' && context.mode === 'default' && !delayed) {
+            delay = context.dataIndex * 300 + context.datasetIndex * 100;
+          }
+          return delay;
+        },
+      },
+      scales: {
+            y: {
+                beginAtZero: true,
+                suggestedMax: 0.3
+            }
+        }
+    }
+});
+const annotation1 = {
+  type: 'line',
+  scaleID: 'x',
+  borderWidth: 3,
+  borderColor: 'black',
+  value: 0.5,
+  label: {
+    content: 'Line annotation at x=0.5',
+    enabled: true
+  },
 };
 
-menu.addEventListener('click', mobileMenu);
-window.addEventListener('scroll', highlightMenu);
-window.addEventListener('click', highlightMenu);
-
-//  Close mobile Menu when clicking on a menu item
-const hideMobileMenu = () => {
-  const menuBars = document.querySelector('.is-active');
-  if (window.innerWidth <= 768 && menuBars) {
-    menu.classList.toggle('is-active');
-    menuLinks.classList.remove('active');
+const annotation2 = {
+  type: 'line',
+  scaleID: 'x',
+  borderWidth: 3,
+  borderColor: 'black',
+  value: 'Label 5',
+  label: {
+    rotation: 'auto',
+    position: 'start',
+    backgroundColor: 'black',
+    content: 'Line at x=Label 5',
+    enabled: true
   }
 };
-
-menuLinks.addEventListener('click', hideMobileMenu);
-navLogo.addEventListener('click', hideMobileMenu);
-
-// For pH Chart in MyWater.html
-const labels = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-];
-
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First dataset',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 20, 30, 45],
-  }]
-};
-
-const config = {
-  type: 'line',
-  data: data,
-  options: {}
-};
-
-const myChart = new Chart(
-  document.getElementById('myChart'),
-  config
-);
+  const myChart4 = new Chart(ctxcon, {
+  type: 'bar',
+  data: {
+    labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    datasets: [{
+      label: 'Average pH',
+      data: [6.8, 7.2, 8.1, 7.9, 8.5, 7.1, 6.2],
+      backgroundColor: [
+        'rgba(54, 162, 235, 0.8)',
+        'rgba(54, 162, 235, 0.8)',
+        'rgba(54, 162, 235, 0.8)',
+        'rgba(54, 162, 235, 0.8)',
+        'rgba(54, 162, 235, 0.8)',
+        'rgba(54, 162, 235, 0.8)',
+        'rgba(54, 162, 235, 0.8)',
+      ],
+      borderColor: [
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(54, 162, 235, 1)'
+      ],
+      borderWidth: 1
+    }]
+      
+  },
+  options: {
+    plugins: {
+      title: {
+        display: true,
+        text: 'pH Indicator',
+        font: {
+          size: 40,
+          family: 'raleway',
+          weight: 'normal',
+        },
+      },
+      annotation: {
+        annotations: {
+          annotation1,
+          annotation2
+        }
+      }
+    },
+    animation: {
+      onComplete: () => {
+        delayed = true;
+      },
+      delay: (context) => {
+        let delay = 0;
+        if (context.type === 'data' && context.mode === 'default' && !delayed) {
+          delay = context.dataIndex * 300 + context.datasetIndex * 100;
+        }
+        return delay;
+      },
+    },
+    scales: {
+          y: {
+              beginAtZero: true,
+              suggestedMax: 10
+          }
+      }
+  }
+  });
